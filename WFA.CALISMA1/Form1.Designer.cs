9daa363545e06383da7ea7cb21ad1b4406bd03a0
ffthylmz <ffthylmz@outlook.com>
2022-07-07 12:14:47 +0300

    Add project files.

---
+++ WFA.CALISMA1/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace WFA.CALISMA1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBotTas = new System.Windows.Forms.Button();
+            this.btnBotKagit = new System.Windows.Forms.Button();
+            this.btnBotMakas = new System.Windows.Forms.Button();
+            this.btnTas = new System.Windows.Forms.Button();
+            this.btnKagit = new System.Windows.Forms.Button();
+            this.btnMakas = new System.Windows.Forms.Button();
+            this.lblMesaj = new System.Windows.Forms.Label();
+            this.lblBotSkor = new System.Windows.Forms.Label();
+            this.lblSkor = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnBotTas
+            // 
+            this.btnBotTas.Enabled = false;
+            this.btnBotTas.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBotTas.Location = new System.Drawing.Point(133, 34);
+            this.btnBotTas.Name = "btnBotTas";
+            this.btnBotTas.Size = new System.Drawing.Size(169, 36);
+            this.btnBotTas.TabIndex = 0;
+            this.btnBotTas.Text = "TAŞ";
+            this.btnBotTas.UseVisualStyleBackColor = true;
+            // 
+            // btnBotKagit
+            // 
+            this.btnBotKagit.Enabled = false;
+            this.btnBotKagit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBotKagit.Location = new System.Drawing.Point(133, 76);
+            this.btnBotKagit.Name = "btnBotKagit";
+            this.btnBotKagit.Size = new System.Drawing.Size(169, 36);
+            this.btnBotKagit.TabIndex = 1;
+            this.btnBotKagit.Text = "KAĞIT";
+            this.btnBotKagit.UseVisualStyleBackColor = true;
+            // 
+            // btnBotMakas
+            // 
+            this.btnBotMakas.Enabled = false;
+            this.btnBotMakas.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBotMakas.Location = new System.Drawing.Point(133, 118);
+            this.btnBotMakas.Name = "btnBotMakas";
+            this.btnBotMakas.Size = new System.Drawing.Size(169, 36);
+            this.btnBotMakas.TabIndex = 2;
+            this.btnBotMakas.Text = "MAKAS";
+            this.btnBotMakas.UseVisualStyleBackColor = true;
+            // 
+            // btnTas
+            // 
+            this.btnTas.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnTas.Location = new System.Drawing.Point(133, 207);
+            this.btnTas.Name = "btnTas";
+            this.btnTas.Size = new System.Drawing.Size(169, 36);
+            this.btnTas.TabIndex = 5;
+            this.btnTas.Text = "TAŞ";
+            this.btnTas.UseVisualStyleBackColor = true;
+            this.btnTas.Click += new System.EventHandler(this.btnTas_Click);
+            // 
+            // btnKagit
+            // 
+            this.btnKagit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKagit.Location = new System.Drawing.Point(133, 249);
+            this.btnKagit.Name = "btnKagit";
+            this.btnKagit.Size = new System.Drawing.Size(169, 36);
+            this.btnKagit.TabIndex = 4;
+            this.btnKagit.Text = "KAĞIT";
+            this.btnKagit.UseVisualStyleBackColor = true;
+            this.btnKagit.Click += new System.EventHandler(this.btnKagit_Click);
+            // 
+            // btnMakas
+            // 
+            this.btnMakas.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnMakas.Location = new System.Drawing.Point(133, 291);
+            this.btnMakas.Name = "btnMakas";
+            this.btnMakas.Size = new System.Drawing.Size(169, 36);
+            this.btnMakas.TabIndex = 3;
+            this.btnMakas.Text = "MAKAS";
+            this.btnMakas.UseVisualStyleBackColor = true;
+            this.btnMakas.Click += new System.EventHandler(this.btnMakas_Click);
+            // 
+            // lblMesaj
+            // 
+            this.lblMesaj.AutoSize = true;
+            this.lblMesaj.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblMesaj.Location = new System.Drawing.Point(147, 175);
+            this.lblMesaj.Name = "lblMesaj";
+            this.lblMesaj.Size = new System.Drawing.Size(0, 17);
+            this.lblMesaj.TabIndex = 6;
+            // 
+            // lblBotSkor
+            // 
+            this.lblBotSkor.AutoSize = true;
+            this.lblBotSkor.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblBotSkor.Location = new System.Drawing.Point(344, 34);
+            this.lblBotSkor.Name = "lblBotSkor";
+            this.lblBotSkor.Size = new System.Drawing.Size(0, 17);
+            this.lblBotSkor.TabIndex = 7;
+            // 
+            // lblSkor
+            // 
+            this.lblSkor.AutoSize = true;
+            this.lblSkor.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSkor.Location = new System.Drawing.Point(344, 207);
+            this.lblSkor.Name = "lblSkor";
+            this.lblSkor.Size = new System.Drawing.Size(0, 17);
+            this.lblSkor.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(462, 367);
+            this.Controls.Add(this.lblSkor);
+            this.Controls.Add(this.lblBotSkor);
+            this.Controls.Add(this.lblMesaj);
+            this.Controls.Add(this.btnTas);
+            this.Controls.Add(this.btnKagit);
+            this.Controls.Add(this.btnMakas);
+            this.Controls.Add(this.btnBotMakas);
+            this.Controls.Add(this.btnBotKagit);
+            this.Controls.Add(this.btnBotTas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBotTas;
+        private System.Windows.Forms.Button btnBotKagit;
+        private System.Windows.Forms.Button btnBotMakas;
+        private System.Windows.Forms.Button btnTas;
+        private System.Windows.Forms.Button btnKagit;
+        private System.Windows.Forms.Button btnMakas;
+        private System.Windows.Forms.Label lblMesaj;
+        private System.Windows.Forms.Label lblBotSkor;
+        private System.Windows.Forms.Label lblSkor;
+    }
+}
+
---
+++ WFA.CALISMA1/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA.CALISMA1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int skor = 0, botSkor = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ButonlariBeyazaCevir();
+        }
+
+        private void btnTas_Click(object sender, EventArgs e)
+        {
+            ButonlariBeyazaCevir();
+            btnTas.BackColor = Color.Yellow;
+            Random rnd = new Random();
+            byte deger = (byte)rnd.Next(1, 4); // 1=BotTaş, 2=BotKağıt, 3=BotMakas
+
+            switch (deger)
+            {
+                case 1:
+                    lblBotSkor.Text = botSkor.ToString();
+                    lblSkor.Text = skor.ToString();
+                    lblMesaj.Text = "Berabere";
+                    btnBotTas.BackColor = Color.Gray;
+
+                    break;
+
+                case 2:
+                    botSkor++;
+                    lblBotSkor.Text = botSkor.ToString();
+                    lblSkor.Text = skor.ToString();
+                    lblMesaj.Text = "Kaybettin";
+                    btnBotKagit.BackColor = Color.Red;
+
+                    break;
+
+                case 3:
+                    skor++;
+                    lblBotSkor.Text = botSkor.ToString();
+                    lblSkor.Text = skor.ToString();
+                    lblMesaj.Text = "Kazandın";
+                    btnBotMakas.BackColor = Color.Green;
+
+                    break;
+
+                default:
+                    break;
+            }
+
+            SkorKontrolEt();
+        }
+
+
+        private void btnKagit_Click(object sender, EventArgs e)
+        {
+            ButonlariBeyazaCevir();
+            btnKagit.BackColor = Color.Yellow;
+            Random rnd = new Random();
+            byte deger = (byte)rnd.Next(1, 4);
+
+            switch (deger)
+            {
+                case 1:
+                    skor++;
+                    lblBotSkor.Text = botSkor.ToString();
+                    lblSkor.Text = skor.ToString();
+                    lblMesaj.Text = "Kazandın";
+                    btnBotTas.BackColor = Color.Green;
+
+                    break;
+
+                case 2:
+                    lblBotSkor.Text = botSkor.ToString();
+                    lblSkor.Text = skor.ToString();
+                    lblMesaj.Text = "Berabere";
+                    btnBotKagit.BackColor = Color.Gray;
+
+                    break;
+
+                case 3:
+                    botSkor++;
+                    lblBotSkor.Text = botSkor.ToString();
+                    lblSkor.Text = skor.ToString();
+                    lblMesaj.Text = "Kaybettin";
+                    btnBotMakas.BackColor = Color.Red;
+
+                    break;
+
+                default:
+                    break;
+            }
+
+            SkorKontrolEt();
+        }
+
+        private void btnMakas_Click(object sender, EventArgs e)
+        {
+            ButonlariBeyazaCevir();
+            btnMakas.BackColor = Color.Yellow;
+            Random rnd = new Random();
+            byte deger = (byte)rnd.Next(1, 4);
+
+            switch (deger)
+            {
+                case 1:
+                    botSkor++;
+                    lblBotSkor.Text = botSkor.ToString();
+                    lblSkor.Text = skor.ToString();
+                    lblMesaj.Text = "Kaybettin";
+                    btnBotTas.BackColor = Color.Red;
+
+                    break;
+
+                case 2:
+                    skor++;
+                    lblBotSkor.Text = botSkor.ToString();
+                    lblSkor.Text = skor.ToString();
+                    lblMesaj.Text = "Kazandın";
+                    btnBotKagit.BackColor = Color.Green;
+
+                    break;
+
+                case 3:
+                    lblBotSkor.Text = botSkor.ToString();
+                    lblSkor.Text = skor.ToString();
+                    lblMesaj.Text = "Berabere";
+                    btnBotMakas.BackColor = Color.Gray;
+
+                    break;
+
+                default:
+                    break;
+            }
+
+            SkorKontrolEt();
+        }
+
+        private void ButonlariBeyazaCevir()
+        {
+            btnKagit.BackColor = Color.White;
+            btnBotKagit.BackColor = Color.White;
+            btnMakas.BackColor = Color.White;
+            btnBotMakas.BackColor = Color.White;
+            btnTas.BackColor = Color.White;
+            btnBotTas.BackColor = Color.White;
+        }
+        private void SkorKontrolEt()
+        {
+            if (lblSkor.Text == 5.ToString() || lblBotSkor.Text == 5.ToString())
+            {
+                ButonlariDisableYap();
+                if (lblSkor.Text == "5")
+                {
+                    lblMesaj.Text = "Sen Kazandın.";
+                }
+                else if (lblBotSkor.Text == "5")
+                {
+                    lblMesaj.Text = "Bot Kazandı.";
+                }
+            }
+        }
+        private void ButonlariDisableYap()
+        {
+            btnKagit.Enabled = false;
+            btnBotKagit.Enabled = false;
+            btnMakas.Enabled = false;
+            btnBotMakas.Enabled = false;
+            btnTas.Enabled = false;
+            btnBotTas.Enabled = false;
+        }
+    }
+}
